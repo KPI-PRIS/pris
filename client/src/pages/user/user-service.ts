388f3561534a3961b45f32f1d45fd6a6661d32d6
Михайло <turictscol40@gmail.com>
2025-01-04 19:06:13 +0200

    add news , list of coaches and player , update ui

--- client/src/pages/user/user-service.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export interface User {
-    id: number;
-    name: string;
-}
-
-export const getUsers =
-    () => axios.get('http://localhost:3000/user/all').then(res => res.data)
