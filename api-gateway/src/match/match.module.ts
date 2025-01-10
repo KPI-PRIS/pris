7a5bace004745497b9e63b51037eb0f7c7380f8e
Михайло <turictscol40@gmail.com>
2025-01-10 18:45:12 +0200

    fix bug

--- api-gateway/src/match/match.module.ts
+++ api-gateway/src/match/match.module.ts
@@ -1,8 +1,10 @@
 import {Module} from '@nestjs/common';
 import {MatchService} from './match.service';
 import {MatchController} from './match.controller';
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
+    imports:[AuthModule],
     controllers: [MatchController],
     providers: [MatchService],
 })
--- api-gateway/src/user/user.module.ts
+++ api-gateway/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import {Module} from '@nestjs/common';
 import {ClientsModule, Transport} from "@nestjs/microservices";
 import {UserController} from "./user.controller";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
     imports: [
+        AuthModule,
         ClientsModule.register([
             {
                 name: 'USER_SERVICE',
